aeedfb3bb474d5c437d298d0271b5476a5f87f23
Gabriele Almeida <naogabe@gmail.com>
2026-09-14 11:38:00 -0300

    feat: add source validation

--- src/modules/sources/dto/create-source.dto.ts
+++ src/modules/sources/dto/create-source.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
 export class CreateSourceDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   type: string;
+
+  @IsUrl()
+  @IsNotEmpty()
   url: string;
 }
