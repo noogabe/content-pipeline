58d9e19aa5aff2b693145a55679df9b422e390bd
Gabriele Almeida <naogabe@gmail.com>
2026-09-14 11:27:40 -0300

    feat: connect sources to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { PrismaModule } from './common/prisma/prisma.module.js';
 import { SourcesModule } from './modules/sources/sources.module.js';
 
 @Module({
-  imports: [SourcesModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    SourcesModule,
+  ],
 })
 export class AppModule {}
--- src/modules/sources/sources.service.ts
+++ src/modules/sources/sources.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import { PrismaService } from '../../common/prisma/prisma.service.js';
+
 @Injectable()
 export class SourcesService {
+  constructor(private readonly prisma: PrismaService) {}
+
   findAll() {
-    return [];
+    return this.prisma.source.findMany();
   }
 }
