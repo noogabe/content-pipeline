f72937337d8e18dc712b382dd2494e4a0532bdc2
Gabriele Almeida <naogabe@gmail.com>
2026-09-14 11:59:13 -0300

    feat: add source activation

--- src/modules/sources/dto/update-source.dto.ts
+++ src/modules/sources/dto/update-source.dto.ts
@@ -1,5 +1,10 @@
+import { IsBoolean, IsOptional } from 'class-validator';
 import { PartialType } from '@nestjs/mapped-types';
 
 import { CreateSourceDto } from './create-source.dto.js';
 
-export class UpdateSourceDto extends PartialType(CreateSourceDto) {}
+export class UpdateSourceDto extends PartialType(CreateSourceDto) {
+  @IsBoolean()
+  @IsOptional()
+  active?: boolean;
+}
