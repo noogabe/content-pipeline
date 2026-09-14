42a465c26d5eb46fde8dfc0cb86f155ebe796884
Gabriele Almeida <naogabe@gmail.com>
2026-09-14 14:59:38 -0300

    feat: add processing module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './common/prisma/prisma.module.js';
 import { SourcesModule } from './modules/sources/sources.module.js';
 import { ArticlesModule } from './modules/articles/articles.module.js';
+import { ProcessingModule } from './modules/processing/processing.module.js';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ArticlesModule } from './modules/articles/articles.module.js';
     PrismaModule,
     SourcesModule,
     ArticlesModule,
+    ProcessingModule,
   ],
 })
 export class AppModule {}
